cda444e9d07f2f2e04755bb0042b544c63148296
Marius van Niekerk <marius.v.niekerk@gmail.com>
2020-08-21 15:23:28 -0400

    bumped version to 0.0.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ REQUIRES = ["click", "requests", "appdirs"]
 
 setup(
     name="ensureconda",
-    version="0.0.3",
+    version="0.0.4",
     description="Install and run applications packaged with conda in isolated environments",
     long_description=readme,
     long_description_content_type="text/markdown",
