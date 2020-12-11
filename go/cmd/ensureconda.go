57af331cf9bd71b4d341a6a6e6bf696576a4218c
Marius van Niekerk <marius.v.niekerk@gmail.com>
2020-12-10 23:01:42 -0500

    Update conda standalone download to match python version

--- go/cmd/ensureconda.go
+++ go/cmd/ensureconda.go
@@ -3,18 +3,22 @@ package cmd
 import (
 	"archive/tar"
 	"compress/bzip2"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/Wessie/appdirs"
 	"github.com/flowchartsman/retry"
 	"github.com/gofrs/flock"
+	"github.com/hashicorp/go-version"
 	"github.com/spf13/cobra"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"sort"
 	"strconv"
 	"strings"
 	"syscall"
@@ -195,24 +199,24 @@ func DownloadToFile(url string, dst string) error {
 	return nil
 }
 
-func InstallCondaStandalone() (string, error) {
-	condaExePrefix := "https://repo.anaconda.com/pkgs/misc/conda-execs"
-	subdir := PlatformSubdir()
-	condaExeName := fmt.Sprintf("conda-latest-%s.exe", subdir)
+//func InstallCondaStandalone() (string, error) {
+//	condaExePrefix := "https://repo.anaconda.com/pkgs/misc/conda-execs"
+//	subdir := PlatformSubdir()
+//	condaExeName := fmt.Sprintf("conda-latest-%s.exe", subdir)
+//
+//	fileUrl := fmt.Sprintf("%s/%s", condaExePrefix, condaExeName)
+//	targetFileName := targetExeFilename("conda_standalone")
+//
+//	err := DownloadToFile(fileUrl, targetFileName)
+//	if err != nil {
+//		return "", err
+//	}
+//	return targetFileName, nil
+//}
 
-	fileUrl := fmt.Sprintf("%s/%s", condaExePrefix, condaExeName)
-	targetFileName := targetExeFilename("conda_standalone")
-
-	err := DownloadToFile(fileUrl, targetFileName)
-	if err != nil {
-		return "", err
-	}
-	return targetFileName, nil
-}
-
-func targetExeFilename(exe_name string) string {
+func targetExeFilename(exeName string) string {
 	_ = os.MkdirAll(sitePath(), 0700)
-	targetFileName := filepath.Join(sitePath(), exe_name)
+	targetFileName := filepath.Join(sitePath(), exeName)
 	if runtime.GOOS == "windows" {
 		targetFileName = targetFileName + ".exe"
 	}
@@ -221,34 +225,81 @@ func targetExeFilename(exe_name string) string {
 
 func InstallMicromamba() (string, error) {
 	url := fmt.Sprintf("https://micromamba.snakepit.net/api/micromamba/%s/latest", PlatformSubdir())
-	targetFileName := targetExeFilename("micromamba")
-
-	retry.NewRetrier(0, 0, 0)
-	retrier := retry.NewRetrier(10, 100*time.Millisecond, 5*time.Second)
-	fileLock := flock.New(targetFileName + ".lock")
-	err := retrier.Run(func() error {
-		locked, err := fileLock.TryLock()
-		if err != nil {
-			return err
-		}
-		if !locked {
-			return errors.New("could not lock")
-		}
-
-		_, errInstall := installMicromamba(url, targetFileName)
-		if errInstall != nil {
-			return errInstall
-		}
-		return nil
-	})
-	if err != nil {
-		return "", err
-	}
-	return targetFileName, nil
-
+	return installMicromamba(url)
 }
 
-func installMicromamba(url string, targetFileName string) (string, error) {
+type AnacondaPkgAttr struct {
+	subdir       string
+	version      string
+	build_number int32
+	timestamp    int32
+	source_url   string
+}
+
+type AnacondaPkg struct {
+	attrs AnacondaPkgAttr
+}
+
+type AnacondaPkgAttrs []AnacondaPkgAttr
+
+func (a AnacondaPkgAttrs) Len() int { return len(a) }
+func (a AnacondaPkgAttrs) Less(i, j int) bool {
+	versioni, _ := version.NewVersion(a[i].version)
+	versionj, _ := version.NewVersion(a[i].version)
+	if versioni.LessThan(versionj) {
+		return true
+	} else if a[i].build_number < a[j].build_number {
+		return true
+	} else if a[i].timestamp < a[j].timestamp {
+		return true
+	}
+	return false
+}
+func (a AnacondaPkgAttrs) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+
+func InstallCondaStandalone() (string, error) {
+	// Get the most recent conda-standalone
+	subdir := PlatformSubdir()
+	url := "https://api.anaconda.org/package/anaconda/conda-standalone/files"
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var data []AnacondaPkg
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var candidates = make([]AnacondaPkgAttr, 0)
+	for _, datum := range data {
+		if datum.attrs.subdir == subdir {
+			candidates = append(candidates, datum.attrs)
+		}
+	}
+	sort.Sort(AnacondaPkgAttrs(candidates))
+
+	chosen := candidates[len(candidates)-1]
+
+	installedExe, err := downloadAndUnpackCondaTarBz2(
+		chosen.source_url, map[string]string{
+			"stanalone_conda/conda.exe": targetExeFilename("conda_standalone"),
+		})
+
+	return installedExe, err
+}
+
+func downloadAndUnpackCondaTarBz2(
+	url string,
+	fileNameMap map[string]string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
@@ -257,7 +308,21 @@ func installMicromamba(url string, targetFileName string) (string, error) {
 
 	bzf := bzip2.NewReader(resp.Body)
 	tarReader := tar.NewReader(bzf)
+	file, err := extractTarFiles(tarReader, fileNameMap)
+	return file, err
+}
 
+func installMicromamba(url string) (string, error) {
+	installedExe, err := downloadAndUnpackCondaTarBz2(
+		url, map[string]string{
+			"Library/bin/micromamba.exe": targetExeFilename("micromamba"),
+			"bin/micromamba":             targetExeFilename("micromamba"),
+		})
+
+	return installedExe, err
+}
+
+func extractTarFiles(tarReader *tar.Reader, fileNameMap map[string]string) (string, error) {
 	for true {
 		header, err := tarReader.Next()
 
@@ -270,49 +335,51 @@ func installMicromamba(url string, targetFileName string) (string, error) {
 
 		switch header.Typeflag {
 		case tar.TypeReg:
-			if header.Name == "Library/bin/micromamba.exe" {
-
+			targetFileName := fileNameMap[header.Name]
+			if targetFileName != "" {
 				err2 := extractTarFile(header, targetFileName, tarReader)
 				if err2 != nil {
 					return "", err2
 				}
 				return targetFileName, nil
-
 			}
-			if header.Name == "bin/micromamba" {
-
-				err2 := extractTarFile(header, targetFileName, tarReader)
-				if err2 != nil {
-					return "", err2
-				}
-
-				return targetFileName, nil
-
-			}
-
 		}
 	}
 	return "", errors.New("could not find file in the tarball")
-
 }
 
 func extractTarFile(header *tar.Header, targetFileName string, tarReader *tar.Reader) error {
-	finfo := header.FileInfo()
-	file, err := os.OpenFile(targetFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, finfo.Mode().Perm())
-	n, cpErr := io.Copy(file, tarReader)
-	if err != nil {
+	fileInfo := header.FileInfo()
+	r := retry.NewRetrier(10, 100*time.Millisecond, 5*time.Second)
+	fileLock := flock.New(targetFileName + ".lock")
+
+	err := r.Run(func() error {
+		locked, err := fileLock.TryLock()
+		if err != nil {
+			return err
+		}
+		if !locked {
+			return errors.New("could not lock")
+		}
+
+		file, err := os.OpenFile(targetFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fileInfo.Mode().Perm())
+		n, cpErr := io.Copy(file, tarReader)
+		if err != nil {
+			return err
+		}
+		if closeErr := file.Close(); closeErr != nil { // close file immediately
+			return closeErr
+		}
+		if cpErr != nil {
+			return cpErr
+		}
+		if n != fileInfo.Size() {
+			return fmt.Errorf("unexpected bytes written: wrote %d, want %d", n, fileInfo.Size())
+		}
 		return err
-	}
-	if closeErr := file.Close(); closeErr != nil { // close file immediately
-		return closeErr
-	}
-	if cpErr != nil {
-		return cpErr
-	}
-	if n != finfo.Size() {
-		return fmt.Errorf("unexpected bytes written: wrote %d, want %d", n, finfo.Size())
-	}
-	return nil
+	})
+
+	return err
 }
 
 // Execute executes the root command.
@@ -329,7 +396,7 @@ func evaluateFlagPair(cmd *cobra.Command, flag string) (bool, error) {
 	posFlag := cmd.Flag(flag)
 	negFlag := cmd.Flag("no-" + flag)
 	if posFlag.Changed && negFlag.Changed {
-		return false, errors.New("Flags are mutually exclusive")
+		return false, errors.New("flags are mutually exclusive")
 	}
 	negVal, err := strconv.ParseBool(negFlag.Value.String())
 	if err != nil {
--- go/go.mod
+++ go/go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/Wessie/appdirs v0.0.0-20141031215813-6573e894f8e2
 	github.com/flowchartsman/retry v0.0.0-20190826180120-7c62e13dbbeb
 	github.com/gofrs/flock v0.8.0
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/hashicorp/go-version v1.2.1
 	github.com/spf13/cobra v1.1.0
-	github.com/spf13/viper v1.7.0 // indirect
 )
