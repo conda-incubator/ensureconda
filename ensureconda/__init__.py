82f1bd8a4a5cfdab46cb2db7be935f31c86be796
Marius van Niekerk <marius.v.niekerk@gmail.com>
2020-12-15 12:59:11 -0500

    Updated github actions used to perform releases

--- ensureconda/__init__.py
+++ ensureconda/__init__.py
@@ -1,4 +1,9 @@
-__version__ = "1.3.0"
-
+import pkg_resources
 
 from .api import ensureconda
+
+
+try:
+    __version__ = pkg_resources.get_distribution("conda_lock").version
+except Exception:
+    __version__ = "unknown"
