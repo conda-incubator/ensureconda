6fc67fd37eefab9fd630d99bc8770387b627a10e
Marius van Niekerk <marius.v.niekerk@gmail.com>
2020-12-15 13:13:35 -0500

    Fixed __init__

--- ensureconda/__init__.py
+++ ensureconda/__init__.py
@@ -4,6 +4,6 @@ from .api import ensureconda
 
 
 try:
-    __version__ = pkg_resources.get_distribution("conda_lock").version
+    __version__ = pkg_resources.get_distribution("ensureconda").version
 except Exception:
     __version__ = "unknown"
